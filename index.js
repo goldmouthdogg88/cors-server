58e21580375ba7813a3f2199b4d72e6e238e33ad
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 18:31:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const req = require("express/lib/request");
+var cors = require("cors");
+const https = require("https");
+const app = express();
+const port = 9000;
+const fs = require("fs");
+
+// var key = fs.readFileSync(__dirname + "/../certs/selfsigned.key");
+// var cert = fs.readFileSync(__dirname + "/../certs/selfsigned.crt");
+
+var key = fs.readFileSync("./selfsigned.key");
+var cert = fs.readFileSync("./selfsigned.crt");
+
+var options = {
+  key: key,
+  cert: cert,
+};
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.get("/", cors(), (req, res) => {
+  res.send("Got a GET cors() request");
+  //   req.json({ msg: "This is CORS-enabled for a single route." });
+});
+
+app.post("/web_history", cors(), (req, res) => {
+  console.log("Got some data!");
+  res.send("Got a POST cors() request");
+
+  //   req.json({ msg: "This is CORS-enabled for a single route." });
+});
+
+var server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log(`Server starting on port : ${port}`);
+});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
